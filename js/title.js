16fe5b1101a68e36e4893828111e7bd35d595652
newZero(local) <804459505@qq.com>
2016-11-05 19:22:23 +0800

    try to another web

--- js/title.js
+++ js/title.js
@@ -1,6 +1,14 @@
+var date = new Date().toString();
+var date2 = date.split(' ');
+document.title = date2[4];
+
 var timer = setInterval(function () {
     var date = new Date().toString();
     var date2 = date.split(' ');
     document.title = date2[4];
 
 },1000)
+
+setTimeout(function () {
+    window.location = 'welcome.html';
+},5000)
